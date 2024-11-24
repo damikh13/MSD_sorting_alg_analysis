00868c57342b6eec193d65157f982bdecb1de66c
Danil Mikhaylov <damikh13@gmail.com>
2024-11-25 02:15:43 +0300

    + opeartion counter class

--- helpers.py
+++ helpers.py
@@ -29,4 +29,3 @@ def save_results_to_csv_same_size(filename, results):
             ])
 def is_sorted(arr):
     return all(arr[i] <= arr[i+1] for i in range(len(arr)-1))
-
---
+++ operation_counter.py
@@ -0,0 +1,6 @@
+class OperationCounter:
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
